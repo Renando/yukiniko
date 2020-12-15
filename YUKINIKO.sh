cf7022a033361d88e064fb0b929526c1ec1ef10d
Du1n9s <58330510+Renando@users.noreply.github.com>
2020-12-16 04:22:08 +0700

    Add files via upload

---
+++ YUKINIKO.sh
@@ -0,0 +1,175 @@
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+#Pm Gua Bangsat
+clear
+cd module
+python2 please.py
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $green
+cat asw.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : DUINGS"
+echo "\033[37;1mFeat: YUKII"
+echo "\033[33;1mKontak Me :  087720646259"
+echo "\033[31;1mPlease For Subcribe Youtube:"
+echo "\033[92mInstagram : renando_okee" 
+echo "\033[36;1mYUKINIKO TOOLS"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mPilih BOT Yang mau di pake:"
+echo "\033[36;1m"
+echo $green "[1] XP"
+echo $purple "[2] CHALIPH"
+echo $cyan "[3] LENT"
+echo $red "[4] MR G3P5"
+echo $white "[5] ARIS"
+echo $green "[6] ABDUL"
+echo $yellow "0.> keluar"
+echo "\033[32;1m"
+read -p ">>>" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "YUKINIKO" | lolcat
+echo $yellow"Menginstall Bot..."
+sleep 2
+git clone https://github.com/XP-TN/XP-TNNBOT
+cd XP-TNNBOT
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+echo $green"BERHASIL[√]"
+echo $green"Silahkan Scan QR ini..."
+sleep 2
+node index.js
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "YUKINIKO"
+echo $yellow"Menginstall Bot..."
+sleep 2
+git clone https://github.com/Caliph71/caliph-bot
+cd caliph-bot
+unzip caliph-bot.zip
+cd caliph-bot
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+echo $green"BERHASIL[√]"
+echo $green"Silahkan Scan QR ini..."
+sleep 2
+node index.js
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "YUKINIKO"
+echo $yellow"Menginstall Bot..."
+sleep 2
+git clone https://github.com/lentbot/LENTBOTV7
+cd LENTBOTV7
+unzip LENTBOTV7.zip
+cd LENTBOTV7
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+echo $green"BERHASIL[√]"
+echo $green"Silahkan Scan QR ini..."
+sleep 2
+node index.js
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "YUKINIKO"
+echo $yellow"Menginstall Bot..."
+sleep 2
+git clone https://github.com/MrG3P5/bot-wacap
+cd bot-wacap
+unzip bot-wacap.zip
+cd bot-wacap
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+echo $green"BERHASIL[√]"
+echo $green"Silahkan Scan QR ini..."
+sleep 2
+node index.js
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "YUKINIKO"
+echo $yellow"Menginstall Bot..."
+sleep 2
+https://github.com/A187ID/AR15BOT
+cd AR15BOT
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+echo $green"BERHASIL[√]"
+echo $green"Silahkan Scan QR ini..."
+sleep 2
+node index.js
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "YUKINIKO"
+echo $yellow"Menginstall Bot..."
+sleep 2
+git clone https://github.com/fdciabdul/termux-whatsapp-bot
+cd termux-whatsapp-bot
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+echo $green"BERHASIL[√]"
+echo $green"Silahkan Scan QR ini..."
+sleep 2
+node index.js
+fi 
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mTHANKS,JAN LUPA BALIK LAGI"
+sleep 1
+figlet "YUKINIKO"
+sleep 1
+echo "\033[33;1mTO BE CONTINUED"
+sleep 1
+figlet "GOODBYE"
+sleep 1
+exit
+fi
+
