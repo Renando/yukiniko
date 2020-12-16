75e8e190eecc5a80e26a796cdbaa09c2b50f63f8
Du1n9s <58330510+Renando@users.noreply.github.com>
2020-12-16 17:03:01 +0700

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,19 @@
+figlet "YUKINIKO" | lolcat
+echo $yellow"Menginstall Bahan Untuk Bot..."
+sleep 1
+echo $yellow"Please Wait..."
+sleep 1
+pkg update && pkg upgrade
+Pkg install git -y
+pkg install tesseract -y
+Pkg install unzip -y
+pkg install nano -y
+Pkg install ffmpeg -y
+Pkg install wget -y
+Pkg install nodejs -y
+Pkg install npm -y
+pkg install python -y
+pkg install python2 -y
+echo $green"BERHASIL[√]"
+sleep 1
+clear
